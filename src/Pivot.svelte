ac0e6dc86e526b1692aa8b5ff09b0a110f0d34e5
Serhii Pylypchuk <pylypchukserhii1991@gmail.com>
2024-09-22 10:38:46 +0400

    [dev] add 'yarn start' command and spaces

--- src/Pivot.svelte
+++ src/Pivot.svelte
@@ -1,59 +1,59 @@
 <script>
-    import { onMount, onDestroy } from "svelte";
-    import { Pivot } from "@dhx/trial-pivot";
-    import "@dhx/trial-pivot/dist/pivot.css";
-    
-    export let fields;
-    export let dataset;
+  import { onMount, onDestroy } from "svelte";
+  import { Pivot } from "@dhx/trial-pivot";
+  import "@dhx/trial-pivot/dist/pivot.css";
+  
+  export let fields;
+  export let dataset;
 
-    let container;
-    let table;
+  let container;
+  let table;
 
-    onMount(() => {
-        table = new Pivot(container, {
-            fields,
-            data: dataset,
-            headerShape: {
-                collapsible: true
-            },
-            tableShape: {
-                tree: true,
-                templates: ["profit", "sales"].reduce((acc, field) => (acc[field] = v => (v ? Math.ceil(v) : ""), acc), {})
-            },
-            columnShape: {
-                autoWidth: {
-                    auto: true,
-                    columns: {
-                        type: true,
-                        title: true
-                    },
-                    firstOnly: false,
-                },
-            },
-            config: {
-                rows: ["state", "product_type"],
-                columns: ["product_line", "type"],
-                values: [
-                    {
-                        field: "profit",
-                        method: "sum"
-                    },
-                    {
-                        field: "sales",
-                        method: "sum"
-                    },
-                    {
-                        field: "date",
-                        method: "min"
-                    }
-                ]
-            }
-        });
+  onMount(() => {
+    table = new Pivot(container, {
+      fields,
+      data: dataset,
+      headerShape: {
+        collapsible: true
+      },
+      tableShape: {
+        tree: true,
+        templates: ["profit", "sales"].reduce((acc, field) => (acc[field] = v => (v ? Math.ceil(v) : ""), acc), {})
+      },
+      columnShape: {
+        autoWidth: {
+          auto: true,
+          columns: {
+            type: true,
+            title: true
+          },
+          firstOnly: false,
+        },
+      },
+      config: {
+        rows: ["state", "product_type"],
+        columns: ["product_line", "type"],
+        values: [
+          {
+            field: "profit",
+            method: "sum"
+          },
+          {
+            field: "sales",
+            method: "sum"
+          },
+          {
+            field: "date",
+            method: "min"
+          }
+        ]
+      }
     });
-    
-    onDestroy(() => {
-        table.destructor();
-    });
+  });
+  
+  onDestroy(() => {
+    table.destructor();
+  });
 </script>
 
 <div bind:this={container} class="widget"></div>
